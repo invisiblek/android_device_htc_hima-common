c38838ee07b272b3070011e234750ea87e43478f
Myles Watson <mylesgw@google.com>
2017-02-02 11:33:19 -0800

    bluetooth: Build the new Bluetooth HAL
    Change-Id: Ibeb69b0764ee6ca3244d4026d69b80fd8ec368b9

--- hima.mk
+++ hima.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Bluetooth HAL
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 # Camera
 PRODUCT_PACKAGES += \
     libshim_camera \
